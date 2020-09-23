6dfc8bd6ef98ecfbd4300818bce2498183976e3b
d.b.huang <d.b.huang@accenture.com>
2020-09-23 10:20:04 +0200

    api get

--- app/views.py
+++ app/views.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello world !"
+
+@app.route('/accueil/<value>/')
+def accueil(value):
+    return value
+
+@app.route('/api_get/', methods=['GET'])
+def api_get():
+    return "my get"
+
+
+
 
 if __name__ == "__main__":
     app.run()
