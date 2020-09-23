6017c055215dd829b93a661f3a0f7967fe5f3f14
d.b.huang <d.b.huang@accenture.com>
2020-09-23 09:48:16 +0200

    update for kata

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,3 @@ from flask import Flask
 from .views import app
 from . import models
 
-# Connect sqlalchemy to app
-models.db.init_app(app)
--- app/models.py
+++ app/models.py
@@ -1,16 +1,16 @@
-from flask_sqlalchemy import SQLAlchemy
-
-from .views import app
-
-# Create database connection object
-db = SQLAlchemy(app)
-
-class Content(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    #description = db.Column(db.String(200), nullable=False)
-    accuracy = db.Column(db.Float(), nullable=False)
-
-    def __init__(self, accuracy):
-            self.accuracy = accuracy
-
-db.create_all()
+# #from flask_sqlalchemy import SQLAlchemy
+#
+# from .views import app
+#
+# # Create database connection object
+# db = SQLAlchemy(app)
+#
+# class Content(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     #description = db.Column(db.String(200), nullable=False)
+#     accuracy = db.Column(db.Float(), nullable=False)
+#
+#     def __init__(self, accuracy):
+#             self.accuracy = accuracy
+#
+# db.create_all()
